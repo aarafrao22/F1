5b1164fe2c83daaee92196169988774f6486e96b
aaraf.younus <aarafrao22@gmail.com>
2023-03-28 14:17:08 +0500

    Init

---
+++ lib/data/question_list.dart
@@ -0,0 +1,24 @@
+import 'package:first_app/model/question_model.dart';
+
+List<QuestionModel> questions = [
+  QuestionModel(
+    "How Many Whisker",
+    {"1": false, "44": false, "2": true, "33": false},
+  ),
+  QuestionModel(
+    "How Many Whisker",
+    {"1": false, "44": false, "2": true, "33": false},
+  ),
+  QuestionModel(
+    "How Many Whisker",
+    {"1": false, "44": false, "2": true, "33": false},
+  ),
+  QuestionModel(
+    "How Many Whisker",
+    {"1": false, "44": false, "2": true, "33": false},
+  ),
+  QuestionModel(
+    "How Many Whisker",
+    {"1": false, "44": false, "2": true, "33": false},
+  ),
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/data/question_list.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,432 +8,61 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return const MaterialApp(
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SimpleLoginScreen(),
     );
   }
 }
 
-/*
-  Flutter UI
-  ----------
-  lib/screens/simple_login.dart
-*/
-
-class SimpleLoginScreen extends StatefulWidget {
-  /// Callback for when this form is submitted successfully. Parameters are (email, password)
-  final Function(String? email, String? password)? onSubmitted;
-
-  const SimpleLoginScreen({this.onSubmitted, Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<SimpleLoginScreen> createState() => _SimpleLoginScreenState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _SimpleLoginScreenState extends State<SimpleLoginScreen> {
-  late String email, password;
-  String? emailError, passwordError;
-
-  Function(String? email, String? password)? get onSubmitted =>
-      widget.onSubmitted;
-
-  @override
-  void initState() {
-    super.initState();
-    email = '';
-    password = '';
-
-    emailError = null;
-    passwordError = null;
-  }
-
-  void resetErrorText() {
-    setState(() {
-      emailError = null;
-      passwordError = null;
-    });
-  }
-
-  bool validate() {
-    resetErrorText();
-
-    RegExp emailExp = RegExp(
-        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
-
-    bool isValid = true;
-    if (email.isEmpty || !emailExp.hasMatch(email)) {
-      setState(() {
-        emailError = 'Email is invalid';
-      });
-      isValid = false;
-    }
-
-    if (password.isEmpty) {
-      setState(() {
-        passwordError = 'Please enter a password';
-      });
-      isValid = false;
-    }
-
-    return isValid;
-  }
-
-  void submit() {
-    if (validate()) {
-      if (onSubmitted != null) {
-        onSubmitted!(email, password);
-      }
-    }
-  }
+class _HomePageState extends State<HomePage> {
+  Color mainColor = Color(0xFF252c4a);
+  Color secondColor = Color(0xFF117eeb);
 
   @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
+      backgroundColor: mainColor,
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: ListView(
-          children: [
-            SizedBox(height: screenHeight * .12),
-            const Text(
-              'Welcome,',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: screenHeight * .01),
-            Text(
-              'Sign in to continue!',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black.withOpacity(.6),
-              ),
-            ),
-            SizedBox(height: screenHeight * .12),
-            InputField(
-              onChanged: (value) {
-                setState(() {
-                  email = value;
-                });
-              },
-              labelText: 'Email',
-              errorText: emailError,
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.next,
-              autoFocus: true,
-            ),
-            SizedBox(height: screenHeight * .025),
-            InputField(
-              onChanged: (value) {
-                setState(() {
-                  password = value;
-                });
-              },
-              onSubmitted: (val) => submit(),
-              labelText: 'Password',
-              errorText: passwordError,
-              obscureText: true,
-              textInputAction: TextInputAction.next,
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed: () {},
-                child: const Text(
-                  'Forgot Password?',
-                  style: TextStyle(
-                    color: Colors.black,
+        padding: EdgeInsets.all(16),
+        child: PageView.builder(
+          itemCount: questions.length,
+          itemBuilder: (context, index) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    "Question ${index + 1}/${questions.length}",
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 28),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(
-              height: screenHeight * .075,
-            ),
-            FormButton(
-              text: 'Log In',
-              onPressed: submit,
-            ),
-            SizedBox(
-              height: screenHeight * .15,
-            ),
-            TextButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => const SimpleRegisterScreen(),
+                const Divider(
+                  color: Colors.white,
+                  height: 8.0,
                 ),
-              ),
-              child: RichText(
-                text: const TextSpan(
-                  text: "I'm a new user, ",
-                  style: TextStyle(color: Colors.black),
-                  children: [
-                    TextSpan(
-                      text: 'Sign Up',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class SimpleRegisterScreen extends StatefulWidget {
-  /// Callback for when this form is submitted successfully.
-  /// Parameters are (email, password)
-
-  final Function(String? email, String? password)? onSubmitted;
-
-  const SimpleRegisterScreen({this.onSubmitted, Key? key}) : super(key: key);
-
-  @override
-  State<SimpleRegisterScreen> createState() => _SimpleRegisterScreenState();
-}
-
-class _SimpleRegisterScreenState extends State<SimpleRegisterScreen> {
-  late String email, password, confirmPassword;
-  String? emailError, passwordError;
-
-  Function(String? email, String? password)? get onSubmitted =>
-      widget.onSubmitted;
-
-  @override
-  void initState() {
-    super.initState();
-    email = '';
-    password = '';
-    confirmPassword = '';
-
-    emailError = null;
-    passwordError = null;
-  }
-
-  void resetErrorText() {
-    setState(() {
-      emailError = null;
-      passwordError = null;
-    });
-  }
-
-  bool validate() {
-    resetErrorText();
-
-    RegExp emailExp = RegExp(
-        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
-
-    bool isValid = true;
-    if (email.isEmpty || !emailExp.hasMatch(email)) {
-      setState(() {
-        emailError = 'Email is invalid';
-      });
-      isValid = false;
-    }
-
-    if (password.isEmpty || confirmPassword.isEmpty) {
-      setState(() {
-        passwordError = 'Please enter a password';
-      });
-      isValid = false;
-    }
-    if (password != confirmPassword) {
-      setState(() {
-        passwordError = 'Passwords do not match';
-      });
-      isValid = false;
-    }
-
-    return isValid;
-  }
-
-  void submit() {
-    if (validate()) {
-      if (onSubmitted != null) {
-        onSubmitted!(email, password);
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: ListView(
-          children: [
-            SizedBox(height: screenHeight * .12),
-            const Text(
-              'Create Account,',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: screenHeight * .01),
-            Text(
-              'Sign up to get started!',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black.withOpacity(.6),
-              ),
-            ),
-            SizedBox(height: screenHeight * .12),
-            InputField(
-              onChanged: (value) {
-                setState(() {
-                  email = value;
-                });
-              },
-              labelText: 'Email',
-              errorText: emailError,
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.next,
-              autoFocus: true,
-            ),
-            SizedBox(height: screenHeight * .025),
-            InputField(
-              onChanged: (value) {
-                setState(() {
-                  password = value;
-                });
-              },
-              labelText: 'Password',
-              errorText: passwordError,
-              obscureText: true,
-              textInputAction: TextInputAction.next,
-            ),
-            SizedBox(height: screenHeight * .025),
-            InputField(
-              onChanged: (value) {
-                setState(() {
-                  confirmPassword = value;
-                });
-              },
-              onSubmitted: (value) => submit(),
-              labelText: 'Confirm Password',
-              errorText: passwordError,
-              obscureText: true,
-              textInputAction: TextInputAction.done,
-            ),
-            SizedBox(
-              height: screenHeight * .075,
-            ),
-            FormButton(
-              text: 'Sign Up',
-              onPressed: submit,
-            ),
-            SizedBox(
-              height: screenHeight * .125,
-            ),
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: RichText(
-                text: const TextSpan(
-                  text: "I'm already a member, ",
-                  style: TextStyle(color: Colors.black),
-                  children: [
-                    TextSpan(
-                      text: 'Sign In',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class FormButton extends StatelessWidget {
-  final String text;
-  final Function? onPressed;
-
-  const FormButton({this.text = '', this.onPressed, Key? key})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-
-    return ElevatedButton(
-      onPressed: onPressed as void Function()?,
-      style: ElevatedButton.styleFrom(
-        padding: EdgeInsets.symmetric(vertical: screenHeight * .02),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-        ),
-      ),
-      child: Text(
-        text,
-        style: const TextStyle(fontSize: 16),
-      ),
-    );
-  }
-}
-
-class InputField extends StatelessWidget {
-  final String? labelText;
-  final Function(String)? onChanged;
-  final Function(String)? onSubmitted;
-  final String? errorText;
-  final TextInputType? keyboardType;
-  final TextInputAction? textInputAction;
-  final bool autoFocus;
-  final bool obscureText;
-
-  const InputField(
-      {this.labelText,
-      this.onChanged,
-      this.onSubmitted,
-      this.errorText,
-      this.keyboardType,
-      this.textInputAction,
-      this.autoFocus = false,
-      this.obscureText = false,
-      Key? key})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      autofocus: autoFocus,
-      onChanged: onChanged,
-      onSubmitted: onSubmitted,
-      keyboardType: keyboardType,
-      textInputAction: textInputAction,
-      obscureText: obscureText,
-      decoration: InputDecoration(
-        labelText: labelText,
-        errorText: errorText,
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
+                Text(questions[index].question!,
+                    style: const TextStyle(fontSize: 28.0, color: Colors.white))
+              ],
+            );
+          },
         ),
       ),
     );
